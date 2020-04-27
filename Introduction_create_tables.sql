d23b35dd4a9b07c1e1195e6be8bd0ad34e295a59
bateiko <bateiko0713@gmail.com>
2020-04-27 17:09:21 +0300

    first tables

--- Introduction_create_tables.sql
+++ Introduction_create_tables.sql
@@ -1,9 +1,18 @@
+DROP DATABASE ukr_zaliznitca;
 CREATE DATABASE ukr_zaliznitca;
 USE ukr_zaliznitca;
-SHOW TABLES;
-CREATE TABLE Employees (Registration_number INT, Full_name CHAR(100), Rang CHAR(50), Department_number INT,
-		Team INT, Count_of_children INT, Sex CHAR, Salary INT, Overview CHAR(3), Age INT);
+# SHOW TABLES; 
+
+CREATE TABLE Ranges (id INT AUTO_INCREMENT PRIMARY KEY, title CHAR(255) NOT NULL);
+CREATE TABLE Department (id INT AUTO_INCREMENT PRIMARY KEY, title CHAR(255) NOT NULL);
+
+CREATE TABLE Employees (id INT PRIMARY KEY AUTO_INCREMENT, full_name CHAR(255), rang_id INT,
+		department_id INT, team_id INT, count_of_children INT,
+        Sex ENUM(), Salary INT, Overview ENUM, Age INT, );
+        
 CREATE TABLE Locomotives (Registration_number INT, Count_of_completed_routes INT, Main_station CHAR(50), Arival_time DATETIME,
 		Last_techical_overview DATE, Last_repair DATE, Repairs_count INT, Locomotiv_age INT,
         Locomotive_team_department_number INT, Locomotive_team_number INT, Tech_team_department_number INT, Tech_team_number INT);
+
+
 drop table Locomotives;
