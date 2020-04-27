9cd25b940fbab2b9a42b3e0b4e071df0dbddb923
bateiko <bateiko0713@gmail.com>
2020-04-27 18:02:59 +0300

    all tables was created

--- Introduction_create_tables.sql
+++ Introduction_create_tables.sql
@@ -1,11 +1,25 @@
 DROP DATABASE ukr_zaliznitca;
 CREATE DATABASE ukr_zaliznitca;
 USE ukr_zaliznitca;
-# SHOW TABLES; 
+
+# DROP TABLE Employees;
+# DROP TABLE Locomotives;
+
+# DROP TABLE Ranges;
+# DROP TABLE Departments;
 
 CREATE TABLE Ranges (id INT AUTO_INCREMENT PRIMARY KEY,
 		title CHAR(255) NOT NULL);
 CREATE TABLE Departments (id INT AUTO_INCREMENT PRIMARY KEY,
+		title CHAR(255) NOT NULL);
+CREATE TABLE Stations (id INT AUTO_INCREMENT PRIMARY KEY,
+		title CHAR(255) NOT NULL);
+CREATE TABLE Delays (id INT AUTO_INCREMENT PRIMARY KEY,
+		title CHAR(255) NOT NULL);
+CREATE TABLE Routs (id INT AUTO_INCREMENT PRIMARY KEY,
+		start_station_id INT NOT NULL,
+        end_station_id INT NOT NULL);
+CREATE TABLE Ticket_statuses(id INT AUTO_INCREMENT PRIMARY KEY,
 		title CHAR(255) NOT NULL);
 
 CREATE TABLE Employees (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
@@ -14,7 +28,7 @@ CREATE TABLE Employees (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 		department_id INT NOT NULL, 
         team_id INT NOT NULL, 
         count_of_children INT,
-        sex ENUM('Чоловік', 'Жінка', 'Не визначився', 'інше'), 
+        sex ENUM('Чоловік', 'Жінка', 'Не визначився', 'інше') NOT NULL, 
         salary INT NOT NULL, 
         overview BOOL NOT NULL, 
         age INT NOT NULL,
@@ -23,24 +37,47 @@ CREATE TABLE Employees (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
         
 CREATE TABLE Locomotives (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL , 
 		count_of_completed_routes INT NOT NULL, 
-        main_station CHAR(255) NOT NULL, 
+        main_station_id INT NOT NULL,
+        current_station_id INT NOT NULL,
         arival_time DATETIME NOT NULL,
 		last_techical_overview DATE , 
         last_repair DATE , 
         repairs_count INT NOT NULL, 
         locomotiv_age INT NOT NULL,
-        locomotive_team_department_number INT NOT NULL, 
-        locomotive_team_number INT NOT NULL, 
-        tech_team_department_number INT NOT NULL, 
-        tech_team_number INT NOT NULL);
+        locomotive_team_department_id INT NOT NULL, 
+        locomotive_team_id INT NOT NULL, 
+        tech_team_department_id INT NOT NULL, 
+        tech_team_id INT NOT NULL,
+        FOREIGN KEY (main_station_id) REFERENCES Stations(id),
+        FOREIGN KEY (current_station_id) REFERENCES Stations(id),
+        FOREIGN KEY (locomotive_team_department_id) REFERENCES Departments(id),
+        FOREIGN KEY (tech_team_department_id) REFERENCES Departments(id));
 
-DROP TABLE Employees;
-DROP TABLE Locomotives;
-
-DROP TABLE Ranges;
-DROP TABLE Departments;
-# DROP TABLE ;
-
-
-
-# drop table Locomotives;
+CREATE TABLE Journeys (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+		rout_id INT NOT NULL,
+        canceled BOOL NOT NULL,
+        delay_id INT NOT NULL,
+        beginning_delay DATETIME,
+        end_delay DATETIME,
+        departure_time DATETIME NOT NULL,
+		arrival_time DATETIME NOT NULL,
+        international_or_internal BOOL NOT NULL,
+        FOREIGN KEY (rout_id) REFERENCES Routs(id),
+        FOREIGN KEY (delay_id) REFERENCES Delays(id));
+        
+CREATE TABLE Tickets (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+	journey_id INT NOT NULL,
+    full_name CHAR(255) NOT NULL,
+    ticket_status_id INT NOT NULL,
+    buying_time DATETIME,
+    return_time DATETIME,
+    price INT NOT NULL,
+    carload_id INT NOT NULL,
+    seat_number INT NOT NULL,
+    sex ENUM('Чоловік', 'Жінка', 'Не визначився', 'інше') NOT NULL,
+    age INT NOT NULL,
+    FOREIGN KEY (journey_id) REFERENCES Journeys(id),
+    FOREIGN KEY (ticket_status_id) REFERENCES Ticket_statuses(id));
+        
+        
+        
