79f58c55a7478f60301dccce77a317f3e00cec1b
bateiko <bateiko0713@gmail.com>
2020-05-05 14:18:04 +0300

    11 was done

--- procedures.sql
+++ procedures.sql
@@ -38,6 +38,16 @@ DROP PROCEDURE IF EXISTS tickets_average_sold_in_period_of_hour_on_some;
 DROP PROCEDURE IF EXISTS tickets_average_sold_price_of_ticket_rout;
 DROP PROCEDURE IF EXISTS journeys_couse_delay_count;
 DROP PROCEDURE IF EXISTS tickets_average_sold_in_period_of_hour_on_some_rout;
+DROP PROCEDURE IF EXISTS journeys_count_of_types_end_station;
+DROP PROCEDURE IF EXISTS journeys_count_of_types;
+DROP PROCEDURE IF EXISTS tickets_count_of_customers_in_journey;
+DROP PROCEDURE IF EXISTS tickets_count_of_customers_in_date_departure;
+DROP PROCEDURE IF EXISTS tickets_count_of_customers_in_date_departure_abroad;
+DROP PROCEDURE IF EXISTS tickets_count_of_customers_handed_luggage;
+DROP PROCEDURE IF EXISTS tickets_count_of_customers_sex;
+DROP PROCEDURE IF EXISTS tickets_count_of_customers_age;
+DROP PROCEDURE IF EXISTS name_proc;
+DROP PROCEDURE IF EXISTS name_proc;
 DROP PROCEDURE IF EXISTS name_proc;
 DROP PROCEDURE IF EXISTS name_proc;
 DROP PROCEDURE IF EXISTS name_proc;
@@ -1210,4 +1220,340 @@ END //
 DELIMITER ;
 
 # CALL tickets_average_sold_price_of_ticket_rout (1,'2020-03-10 12:45:30','2020-03-10 19:45:30',500,@COUNT_OF_BUY_TICKETS);
+
+DELIMITER //
+CREATE PROCEDURE journeys_count_of_types (IN type_journey_id INT, IN type_train_id INT, OUT count_routs INT)
+BEGIN 
+	SELECT routs.id AS 'Номер рейсу',
+			stations.title AS 'Початкова станція',
+			stations2.title AS 'Кінцева станція',
+			IF (journeys.canceled,'Так','Ні') AS 'Чи відмінений',
+			delays.title AS 'Причина затримки',
+			IF (journeys.beginning_delay IS NOT NULL,journeys.beginning_delay,'-') AS 'Початок затримки',
+			IF (journeys.end_delay IS NOT NULL, journeys.end_delay,'-') AS 'Кінець затримки',
+			journeys.departure_time AS 'Час відправлення',
+			journeys.arrival_time AS 'Час прибуття',
+			journeys.price_ticket AS 'Ціна за квиток',
+			types_journey.title AS 'Тип маршруту',
+			types_train.title AS 'Тип потяга'
+	FROM journeys 
+	LEFT JOIN delays ON journeys.delay_id = delays.id
+	LEFT JOIN routs ON journeys.rout_id = routs.id
+	LEFT JOIN stations ON routs.start_station_id = stations.id
+	LEFT JOIN stations stations2 ON routs.end_station_id = stations2.id
+	LEFT JOIN types_journey ON journeys.type_journey = types_journey.id
+	LEFT JOIN types_train ON journeys.type_train = types_train.id
+    WHERE journeys.type_train = type_train_id 
+    AND journeys.type_journey = type_journey_id;
+
+    SELECT COUNT(*) INTO count_routs 
+	FROM journeys 
+	WHERE journeys.type_train = type_train_id 
+    AND journeys.type_journey = type_journey_id;
+    
+	SELECT count_routs;
+END //
+DELIMITER ;
+
+# CALL journeys_count_of_types(1,1,@count_routs);
+
+DELIMITER //
+CREATE PROCEDURE journeys_count_of_types_end_station (IN type_journey_id INT, IN type_train_id INT, IN end_station_id1 int, OUT count_routs INT)
+BEGIN 
+	SELECT routs.id AS 'Номер рейсу',
+			stations.title AS 'Початкова станція',
+			stations2.title AS 'Кінцева станція',
+			IF (journeys.canceled,'Так','Ні') AS 'Чи відмінений',
+			delays.title AS 'Причина затримки',
+			IF (journeys.beginning_delay IS NOT NULL,journeys.beginning_delay,'-') AS 'Початок затримки',
+			IF (journeys.end_delay IS NOT NULL, journeys.end_delay,'-') AS 'Кінець затримки',
+			journeys.departure_time AS 'Час відправлення',
+			journeys.arrival_time AS 'Час прибуття',
+			journeys.price_ticket AS 'Ціна за квиток',
+			types_journey.title AS 'Тип маршруту',
+			types_train.title AS 'Тип потяга'
+	FROM journeys 
+	LEFT JOIN delays ON journeys.delay_id = delays.id
+	LEFT JOIN routs ON journeys.rout_id = routs.id
+	LEFT JOIN stations ON routs.start_station_id = stations.id
+	LEFT JOIN stations stations2 ON routs.end_station_id = stations2.id
+	LEFT JOIN types_journey ON journeys.type_journey = types_journey.id
+	LEFT JOIN types_train ON journeys.type_train = types_train.id
+    WHERE journeys.type_train = type_train_id 
+    AND journeys.type_journey = type_journey_id
+    AND routs.end_station_id = end_station_id1;
+
+    SELECT COUNT(*) INTO count_routs 
+	FROM ( SELECT routs.id AS rout_id,
+				stations.id AS stat_start,
+				stations2.id AS stat_end, 
+				types_journey.id AS t_journey,
+				types_train.id AS t_train
+			FROM journeys 
+            LEFT JOIN routs ON journeys.rout_id = routs.id
+            LEFT JOIN stations ON routs.start_station_id = stations.id
+			LEFT JOIN stations stations2 ON routs.end_station_id = stations2.id
+            LEFT JOIN types_journey ON journeys.type_journey = types_journey.id
+			LEFT JOIN types_train ON journeys.type_train = types_train.id) AS a
+	WHERE a.t_journey = type_journey_id
+    AND a.t_train = type_train_id
+    AND a.stat_end = end_station_id1;
+    
+	SELECT count_routs;
+END //
+DELIMITER ;
+
+# CALL journeys_count_of_types_end_station(1,1,98,@count_routs);
+
+DELIMITER //
+CREATE PROCEDURE tickets_count_of_customers_in_journey (IN journey_id INT, OUT count_tickets INT)
+BEGIN 
+	SELECT tickets.id,
+			tickets.full_name AS 'ПІБ',
+			journeys.id AS 'Номер рейсу',
+			tickets.carload_id AS 'Номер вагону',
+			tickets.seat_number AS 'Номер місця',
+			tickets.price AS 'Ціна',
+			stations.title AS 'Початкова станція',
+			stations2.title AS 'Кінцева станція',
+			tickets.ticket_status AS 'Статус квитка',
+			IF (tickets.buying_time IS NOT NULL,tickets.buying_time, '-') AS 'Час купівлі',
+			IF (tickets.return_time IS NOT NULL,tickets.return_time, '-') AS 'Час повернення',
+			tickets.sex AS 'Стать',
+			tickets.age AS 'Вік',
+            IF (tickets.handed_luggage,'Так','Ні') AS 'Зданий багаж'
+	FROM tickets
+	LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id
+	LEFT JOIN routs ON tickets.journey_id = routs.id
+	LEFT JOIN stations ON routs.start_station_id = stations.id
+	LEFT JOIN stations stations2 ON routs.end_station_id = stations2.id
+    WHERE journeys.id = journey_id;
+    
+    SELECT COUNT(*) INTO count_tickets FROM 
+    (	SELECT journeys.id AS j_id, 
+				tickets.id AS tick_id 
+		FROM tickets
+		LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id
+		WHERE journeys.id = journey_id ) AS a
+    WHERE a.j_id = journey_id;
+    
+    SELECT count_tickets;
+END //
+DELIMITER ;
+
+# CALL tickets_count_of_customers_in_journey(1,@count_customers);
+
+DELIMITER //
+CREATE PROCEDURE tickets_count_of_customers_in_date_departure (IN journey_id INT,IN current_day DATE, OUT count_tickets INT)
+BEGIN 
+	SELECT tickets.id,
+			tickets.full_name AS 'ПІБ',
+			journeys.id AS 'Номер рейсу',
+			tickets.carload_id AS 'Номер вагону',
+			tickets.seat_number AS 'Номер місця',
+			tickets.price AS 'Ціна',
+			stations.title AS 'Початкова станція',
+			stations2.title AS 'Кінцева станція',
+			tickets.ticket_status AS 'Статус квитка',
+			IF (tickets.buying_time IS NOT NULL,tickets.buying_time, '-') AS 'Час купівлі',
+			IF (tickets.return_time IS NOT NULL,tickets.return_time, '-') AS 'Час повернення',
+			tickets.sex AS 'Стать',
+			tickets.age AS 'Вік',
+            IF (tickets.handed_luggage,'Так','Ні') AS 'Зданий багаж'
+	FROM tickets
+	LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id
+	LEFT JOIN routs ON tickets.journey_id = routs.id
+	LEFT JOIN stations ON routs.start_station_id = stations.id
+	LEFT JOIN stations stations2 ON routs.end_station_id = stations2.id
+    WHERE DATE(journeys.departure_time) = current_day
+    AND journeys.id = journey_id
+    AND tickets.ticket_status = 'куплено';
+    
+    SELECT COUNT(*) INTO count_tickets FROM 
+    (	SELECT journeys.id AS j_id, 
+				tickets.id AS tick_id,
+                DATE(journeys.departure_time) AS d_time,
+                DATE(journeys.arrival_time) AS a_time,
+                tickets.ticket_status AS t_status
+		FROM tickets
+		LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id) AS a
+	WHERE a.d_time = current_day 
+    AND a.j_id = journey_id
+    AND a.t_status = 'куплено';
+
+    SELECT count_tickets;
+END //
+DELIMITER ;
+
+# CALL tickets_count_of_customers_in_date_departure(1,'2020-03-13',@count_customers);
+
+DELIMITER //
+CREATE PROCEDURE tickets_count_of_customers_in_date_departure_abroad (IN journey_id INT, IN current_day DATE, OUT count_tickets INT)
+BEGIN 
+	SELECT tickets.id,
+			tickets.full_name AS 'ПІБ',
+			journeys.id AS 'Номер рейсу',
+			tickets.carload_id AS 'Номер вагону',
+			tickets.seat_number AS 'Номер місця',
+			tickets.price AS 'Ціна',
+			stations.title AS 'Початкова станція',
+			stations2.title AS 'Кінцева станція',
+			tickets.ticket_status AS 'Статус квитка',
+			IF (tickets.buying_time IS NOT NULL,tickets.buying_time, '-') AS 'Час купівлі',
+			IF (tickets.return_time IS NOT NULL,tickets.return_time, '-') AS 'Час повернення',
+			tickets.sex AS 'Стать',
+			tickets.age AS 'Вік',
+            IF (tickets.handed_luggage,'Так','Ні') AS 'Зданий багаж'
+	FROM tickets
+	LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id
+	LEFT JOIN routs ON tickets.journey_id = routs.id
+	LEFT JOIN stations ON routs.start_station_id = stations.id
+	LEFT JOIN stations stations2 ON routs.end_station_id = stations2.id
+    WHERE DATE(journeys.departure_time) = current_day
+    AND journeys.type_journey = 2
+    AND journeys.id = journey_id
+	AND tickets.ticket_status = 'куплено';
+    
+    SELECT COUNT(*) INTO count_tickets FROM 
+    (	SELECT journeys.id AS j_id, 
+				tickets.id AS tick_id,
+                DATE(journeys.departure_time) AS d_time,
+                DATE(journeys.arrival_time) AS a_time,
+                journeys.type_journey AS t_journey,
+                tickets.ticket_status AS t_status
+		FROM tickets
+		LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id) AS a
+	WHERE a.d_time = current_day 
+	AND a.t_journey = 2
+    AND a.j_id = journey_id
+    AND a.t_status = 'куплено';
+    
+    SELECT count_tickets;
+END //
+DELIMITER ;
+
+# CALL tickets_count_of_customers_in_date_departure_abroad(6,'2020-03-09',@count_customers);
+
+DELIMITER //
+CREATE PROCEDURE tickets_count_of_customers_handed_luggage (IN journey_id INT, OUT count_tickets INT)
+BEGIN 
+	SELECT tickets.id,
+			tickets.full_name AS 'ПІБ',
+			journeys.id AS 'Номер рейсу',
+			tickets.carload_id AS 'Номер вагону',
+			tickets.seat_number AS 'Номер місця',
+			tickets.price AS 'Ціна',
+			stations.title AS 'Початкова станція',
+			stations2.title AS 'Кінцева станція',
+			tickets.ticket_status AS 'Статус квитка',
+			IF (tickets.buying_time IS NOT NULL,tickets.buying_time, '-') AS 'Час купівлі',
+			IF (tickets.return_time IS NOT NULL,tickets.return_time, '-') AS 'Час повернення',
+			tickets.sex AS 'Стать',
+			tickets.age AS 'Вік',
+            IF (tickets.handed_luggage,'Так','Ні') AS 'Зданий багаж'
+	FROM tickets
+	LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id
+	LEFT JOIN routs ON tickets.journey_id = routs.id
+	LEFT JOIN stations ON routs.start_station_id = stations.id
+	LEFT JOIN stations stations2 ON routs.end_station_id = stations2.id
+    WHERE tickets.journey_id = journey_id
+    AND tickets.handed_luggage = 1;
+    
+    SELECT COUNT(*) INTO count_tickets FROM 
+    (	SELECT journeys.id AS j_id, 
+				tickets.id AS tick_id,
+                DATE(journeys.departure_time) AS d_time,
+                DATE(journeys.arrival_time) AS a_time,
+                tickets.handed_luggage AS h_laguage
+		FROM tickets
+		LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id) AS a
+	WHERE a.j_id = journey_id 
+	AND a.h_laguage = 1;
+    
+    SELECT count_tickets;
+END //
+DELIMITER ;
+
+# CALL tickets_count_of_customers_handed_luggage(1,@count_customers);
+
+DELIMITER //
+CREATE PROCEDURE tickets_count_of_customers_sex (IN journey_id INT,IN sex INT, OUT count_tickets INT)
+BEGIN 
+	SELECT tickets.id,
+			tickets.full_name AS 'ПІБ',
+			journeys.id AS 'Номер рейсу',
+			tickets.carload_id AS 'Номер вагону',
+			tickets.seat_number AS 'Номер місця',
+			tickets.price AS 'Ціна',
+			stations.title AS 'Початкова станція',
+			stations2.title AS 'Кінцева станція',
+			tickets.ticket_status AS 'Статус квитка',
+			IF (tickets.buying_time IS NOT NULL,tickets.buying_time, '-') AS 'Час купівлі',
+			IF (tickets.return_time IS NOT NULL,tickets.return_time, '-') AS 'Час повернення',
+			tickets.sex AS 'Стать',
+			tickets.age AS 'Вік',
+            IF (tickets.handed_luggage,'Так','Ні') AS 'Зданий багаж'
+	FROM tickets
+	LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id
+	LEFT JOIN routs ON tickets.journey_id = routs.id
+	LEFT JOIN stations ON routs.start_station_id = stations.id
+	LEFT JOIN stations stations2 ON routs.end_station_id = stations2.id
+    WHERE tickets.journey_id = journey_id
+    AND tickets.sex = sex;
+    
+    SELECT COUNT(*) INTO count_tickets FROM 
+    (	SELECT journeys.id AS j_id, 
+				tickets.id AS tick_id,
+                tickets.sex AS t_sex,
+                tickets.handed_luggage AS h_laguage
+		FROM tickets
+		LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id) AS a
+	WHERE a.j_id = journey_id 
+	AND a.t_sex = sex;
+    
+    SELECT count_tickets;
+END //
+DELIMITER ;
+
+# CALL tickets_count_of_customers_sex(1,1,@count_customers);
+
+DELIMITER //
+CREATE PROCEDURE tickets_count_of_customers_age (IN journey_id INT,IN age INT, OUT count_tickets INT)
+BEGIN 
+	SELECT tickets.id,
+			tickets.full_name AS 'ПІБ',
+			journeys.id AS 'Номер рейсу',
+			tickets.carload_id AS 'Номер вагону',
+			tickets.seat_number AS 'Номер місця',
+			tickets.price AS 'Ціна',
+			stations.title AS 'Початкова станція',
+			stations2.title AS 'Кінцева станція',
+			tickets.ticket_status AS 'Статус квитка',
+			IF (tickets.buying_time IS NOT NULL,tickets.buying_time, '-') AS 'Час купівлі',
+			IF (tickets.return_time IS NOT NULL,tickets.return_time, '-') AS 'Час повернення',
+			tickets.sex AS 'Стать',
+			tickets.age AS 'Вік',
+            IF (tickets.handed_luggage,'Так','Ні') AS 'Зданий багаж'
+	FROM tickets
+	LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id
+	LEFT JOIN routs ON tickets.journey_id = routs.id
+	LEFT JOIN stations ON routs.start_station_id = stations.id
+	LEFT JOIN stations stations2 ON routs.end_station_id = stations2.id
+    WHERE tickets.journey_id = journey_id
+    AND (tickets.age BETWEEN age - 2 AND age + 2);
+    
+    SELECT COUNT(*) INTO count_tickets FROM 
+    (	SELECT journeys.id AS j_id, 
+				tickets.id AS tick_id,
+                tickets.age AS t_age
+		FROM tickets
+		LEFT JOIN journeys ON journeys.rout_id = tickets.journey_id) AS a
+	WHERE a.j_id = journey_id 
+	AND (a.t_age BETWEEN age - 2 AND age + 2);
+    
+    SELECT count_tickets;
+END //
+DELIMITER ;
+
+# CALL tickets_count_of_customers_age(1,45,@count_customers);
 
